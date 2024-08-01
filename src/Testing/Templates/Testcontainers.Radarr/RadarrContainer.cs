28279430dcffe21318fcb614717844ee3ad0e971
Max T. Kristiansen <me@maxtrier.dk>
2024-08-01 12:20:17 +0200

    feat(testing): Added TestContainer module for Radarr

---
+++ src/Testing/Templates/Testcontainers.Radarr/RadarrContainer.cs
@@ -0,0 +1,14 @@
+namespace Fetcharr.Testing.Templates.Testcontainers.Radarr
+{
+    /// <inheritdoc cref="DockerContainer" />
+    /// <summary>
+    ///   Initializes a new instance of the <see cref="RadarrContainer" /> class.
+    /// </summary>
+    /// <param name="configuration">The container configuration.</param>
+    public sealed class RadarrContainer(
+        RadarrConfiguration configuration)
+        : DockerContainer(configuration)
+    {
+        public readonly RadarrConfiguration Configuration = configuration;
+    }
+}
